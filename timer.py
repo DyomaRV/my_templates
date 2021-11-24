c93cc276776514d5f742752ef2fbf591c4b90283
Dmitriy <dyoma@list.ru>
2021-11-24 15:35:56 +0300

    add timer

---
+++ timer.py
@@ -0,0 +1,23 @@
+import time
+from typing import  Callable, Any
+
+
+def timer(func: Callable, *args, **kwargs) -> Any:
+    """Функция таймер. Выводит время работы функции и возвращает ее результат"""
+    started_at = time.time()
+    result = func(*args, **kwargs)
+    ended_at = time.time()
+    run_time = round(ended_at - started_at, 3)
+    print('Время работы функции {} сек.'.format(run_time))
+    return result                                                           # результат работы функции func
+
+
+def cubes_sum(number: int) -> int:
+    result = 0
+    for _ in range(number + 1):
+        result += sum([num ** 3 for num in range(100000)])
+    return result
+
+
+my_result = timer(cubes_sum, 100)
+print(my_result)
