ad2f31193e4cd73f17047b7ab33fb22e51a4460e
Dmitriy <dyoma@list.ru>
2021-11-24 12:07:27 +0300

    Add iter, generator

--- iterator_generator.py
+++ iterator_generator.py
@@ -1,16 +1,19 @@
-class Iterator:
-    def __init__(self, data: list):
-        self.list = data
-        self.counter = -1
-        self.len = len(data)
+from _collections_abc import Iterable
 
-    def __iter__(self):
+
+class Iterator:
+    def __init__(self, data: list) -> None:                 # class constructor
+        self.list = data                                    # assigning an itter object
+        self.counter = -1                                   # counter of iterations
+        self.len = len(data)                                # the length of the list, for optimization
+
+    def __iter__(self) -> Iterable:                         # iterator method
         return self
 
-    def __next__(self):
-        while self.counter < self.len - 1:
-            self.counter += 1
-            return self.list[self.counter]
+    def __next__(self) -> any:                              # next step of itter
+        while self.counter < self.len - 1:                  # control of the number of iterations
+            self.counter += 1                               # counter of iterations
+            return self.list[self.counter]                  # return next index of list
         raise StopIteration
 
 
@@ -21,10 +24,12 @@ def gen(data):
 
 a = [1, 2, 3, 4, 5, 6, 7, 8]
 a_iter = Iterator(data=a)
-a_gen = gen(data=a)
-for line in a_gen:
+print(type(a_iter))
+for line in Iterator(data=a):
     print(line, end=' ')
 
 print()
-for line in Iterator(data=a):
+a_gen = gen(data=a)
+print(type(a_gen))
+for line in a_gen:
     print(line, end=' ')
