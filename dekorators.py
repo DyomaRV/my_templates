0f884e9b147693dd0af4e32d813dc3fb9a051f01
dyoma <dyoma@list.ru>
2021-11-27 17:34:38 +0300

    add class_MRO, search_text_in_file

--- dekorators.py
+++ dekorators.py
@@ -21,7 +21,33 @@ def greeting(name):
 print(greeting.__name__)                                                # без @functools.wraps(func) в декораторе не
 print(greeting.__doc__)                                                 #  работает выведет значения для def wrapped_func
 ######################################################################################################################
+from typing import  Callable, Any, Optional
+def repeats(_func: Optional[Callable] = None, *, num_of_repeat: int = 10) -> Callable:
+    """Функция если принимает num_of_repeat то передает его декоратору.
+        если num_of_repeat отсутсвует, срабатывает параметр num_of_repeat по умолчанию
+    """
+    def do_repeat(func: Callable) -> Callable:
+        """Декоратор, исполняет декорируемую функцию дважды num_of_repeat раз"""
+        def wrapped_func(*args, **kwargs) -> Any:
+            for _ in range(num_of_repeat):
+                func(*args, **kwargs)
+            return
+        return wrapped_func
+    if _func is None:
+        return do_repeat
+    return do_repeat(_func)
 
+
+@repeats(num_of_repeat=5)
+def greeting(name):
+    print('Привет, {name}!'.format(name=name))
+greeting('Tom')
+
+@repeats
+def greeting_(name):
+    print('Привет, {name}!'.format(name=name))
+greeting_('Bob')
+######################################################################################################################
 
 def timer(func: Callable, *args, **kwargs) -> Any:
     """Декоратор таймер. Выводит время работы функции и возвращает ее результат"""
@@ -98,3 +124,107 @@ def counter(func: Callable) -> Callable:
         result = func(*args, **kwargs)
         return result
     return wrapped_func
+######################################################################################################################
+import time
+from datetime import datetime
+from typing import Callable, Any
+import functools
+
+def create_time(cls: Callable) -> Callable:
+    """Декоратор, выводит время создания инстанса класса"""
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs) -> Any:
+        instance = cls(*args, **kwargs)
+        print('\nВремя создания инстанса класса', datetime.utcnow())
+        print('Методы сласса {}'.format(str(cls)[str(cls).rfind('.')+1:len(str(cls))-2]))
+        for i_method in dir(cls):
+            if i_method.startswith('__'):
+                pass
+            else:
+                print('{}'.format(i_method), end=' ')
+        return instance
+    return wrapper
+
+
+@create_time
+class Square:
+######################################################################################################################
+import os
+def for_all_methods(decorator: Callable) -> Callable:
+    """
+    Декоратор класса:
+    Получает другой декоратор и применяет
+     его ко все методам класса
+    """
+
+    @functools.wraps(decorator)
+    def decorate(cls):
+        print('for_all_methods')
+        for i_method in dir(cls):
+            if i_method.startswith('__'):
+                pass
+            else:
+                cur_method = getattr(cls, i_method)
+                decorator_method = decorator(cur_method)
+                setattr(cls, i_method, decorator_method)
+        return cls
+
+    return decorate
+
+def logging(func: Callable, *args, **kwargs) -> Any:
+    """
+    Декоратор записывает в файл log.txt дату время и результат
+    выполнения метода
+    """
+
+    def wrapped_func(*args, **kwargs):
+        # path = os.getcwd()
+        # print(path)
+        with open('/home/dyoma/ARB/Python/skillBox/Python_Basic/Lessons/task_29_4/log.txt', 'a',
+                  encoding='utf8') as log_file:
+            result = func(*args, **kwargs)
+            log_file.write('{} {}\n'.format(datetime.utcnow(), result))
+            return result  # результат работы функции func
+
+    return wrapped_func
+
+@for_all_methods(logging)
+class Square:
+    """
+    Класс фигуры квадрат. Геттер и сеттер на длину стороны.
+    Arg:
+        length - длина стороны квадрата
+    Arguments:
+        __length - инкапсулированная длина стороны квадрата
+    """
+
+    def __init__(self, length: float) -> None:
+        self.length = length
+
+    @property
+    def length(self) -> float:
+        """Геттер длины стороны квадрата"""
+        return self.__length
+
+    @length.setter
+    def length(self, new_length) -> None:
+        """Сеттер длины стороны квадрата"""
+        self.__length = new_length
+
+    def square(self) -> float:
+        """Возвращает площадь квадрата"""
+        return self.length ** 2
+
+    def perimeter(self) -> float:
+        """Возвращает периметр квадрата"""
+        return self.length * 4
+
+path = os.getcwd()
+print(path)
+
+square_1 = Square(length=10)
+square_2 = Square(length=5)
+print(square_1.square())
+print(square_2.square())
+print(square_1.perimeter())
+print(square_2.perimeter())
--- timer.py
+++ timer.py
@@ -1,5 +1,5 @@
 import time
-from typing import  Callable, Any
+from typing import Callable, Any
 
 
 def timer(func: Callable, *args, **kwargs) -> Any:
@@ -21,3 +21,23 @@ def cubes_sum(number: int) -> int:
 
 my_result = timer(cubes_sum, 100)
 print(my_result)
+
+########################################################################################################################
+from _collections_abc import Iterator
+from contextlib import contextmanager
+
+
+@contextmanager
+def timer() -> Iterator:
+    """Функция контекст менеджер. Считает время выполнения кода"""
+    start = time.time()
+    try:
+        yield
+    finally:
+        print('timer = ', time.time() - start)
+
+
+with timer() as t1:
+    print('Первая часть')
+    val_1 = 100 * 100 ** 1000000
+    # какой то код
